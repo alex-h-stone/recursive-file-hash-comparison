e0ce1db20e88afdea33690d1bb01f87367b32557
Alex Stone <148394432+alex-h-stone@users.noreply.github.com>
2023-12-30 15:25:03 +0000

    Fix Serialisation issue

--- src/main/java/dev/alexhstone/model/GsonFactory.java
+++ src/main/java/dev/alexhstone/model/GsonFactory.java
@@ -2,6 +2,7 @@ package dev.alexhstone.model;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import dev.alexhstone.model.datastore.HashResult;
 import dev.alexhstone.model.datastore.HashResultSerializerAndDeserializer;
 import dev.alexhstone.model.queue.WorkItem;
 import dev.alexhstone.model.queue.WorkItemGsonSerializerAndDeserializer;
@@ -15,7 +16,7 @@ public class GsonFactory {
                 .setPrettyPrinting()
                 .registerTypeAdapter(WorkItem.class,
                         new WorkItemGsonSerializerAndDeserializer())
-                .registerTypeAdapter(WorkItem.class,
+                .registerTypeAdapter(HashResult.class,
                         new HashResultSerializerAndDeserializer())
                 .create();
     }
